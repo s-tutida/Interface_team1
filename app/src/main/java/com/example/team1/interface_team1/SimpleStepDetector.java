bdff2863c3154cf44ed83fa7f59a035a87645f07
Sakai Takashi <miimou@gmail.com>
2017-08-08 17:27:58 +0900

    ライセンス準拠を修正

--- app/src/main/java/com/example/team1/interface_team1/SimpleStepDetector.java
+++ app/src/main/java/com/example/team1/interface_team1/SimpleStepDetector.java
@@ -1,7 +1,29 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/*
+ * Modifications by s-tutida in 2017/08/{06-07}:
+ *   - change package name
+ */
+
 package com.example.team1.interface_team1;
 
 /**
- * Created by s-tutida on 2017/08/06.
+ * Added to the codebase by s-tutida on 2017/08/06.
+ *
+ * Receives sensor updates and alerts a StepListener when a step has been detected.
  */
 
 public class SimpleStepDetector {
